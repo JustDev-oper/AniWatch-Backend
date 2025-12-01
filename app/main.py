e5b7d19f4b8d746c98fb306f4c44dc1235fa7f84
dead.code <itoppro11@gmail.com>
2025-12-01 15:59:04 +0300

    feat: Добавил CORS

--- app/main.py
+++ app/main.py
@@ -3,8 +3,17 @@ from fastapi.staticfiles import StaticFiles
 import os
 from app.api.v1.anime import router
 from app.database.session import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
 
 app: FastAPI = FastAPI(title="AniWatch Backend")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 app.include_router(router, prefix="/api/v1")
